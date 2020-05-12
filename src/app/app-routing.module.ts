0a450066ac202dc98db664fccb76f4e388714f05
Anh3h <courageangeh@gmail.com>
2020-05-12 14:34:35 -0400

    Fix file names

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { EmployeesContainerComponent } from './employees/container/employees-container.component';
+import { EmployeesContainerComponent } from './employee/containers/employees-container.component';
 
 const routes: Routes = [
   { path: '', component:  HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { AppComponent } from './app.component';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
-import { EmployeesContainerComponent } from './employees/container/employees-container.component';
-import { EmployeesComponent } from './employees/component/employees.component';
+import { EmployeesContainerComponent } from './employee/containers/employees-container.component';
+import { EmployeesComponent } from './employee/components/employees.component';
 
 @NgModule({
   declarations: [
